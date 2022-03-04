5bac0b720b0c57356a3273e9aab43f9fa55b24d8
Daniel González Peralta <daniel.gonzalez@enti.cat>
2022-03-04 14:18:13 +0100

    ZombieBreaker_v0.6

--- Pepo-productions-game-Pruebas-detecci-n-enemigos/Pepo productions game/Assets/Scripts/Player/CameraMovement.cs
+++ Pepo-productions-game-Pruebas-detecci-n-enemigos/Pepo productions game/Assets/Scripts/Player/CameraMovement.cs
@@ -7,18 +7,25 @@ public class CameraMovement : MonoBehaviour
     public Transform target;
 
     public float cameraSpeed;
+    public float cameraToMouseSpeed;
 
-    public float rotation;
-
-    private Vector2 mousePosition;
+    private Vector2 mousePosition, destiny;
     void Update()
     {
         mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 destiny = (mousePosition - (Vector2)target.position) / 5;
+        destiny = (mousePosition - (Vector2)target.position) / 8;
 
         float xdistanceToDestiny = (destiny.x - transform.position.x) + target.position.x;
         float ydistanceToDestiny = (destiny.y - transform.position.y) + target.position.y;
 
-        transform.Translate(xdistanceToDestiny * cameraSpeed * Time.deltaTime, ydistanceToDestiny * cameraSpeed * Time.deltaTime, 0);
+        float xDistancetoTarget = (target.position.x - transform.position.x) / 5;
+        float yDistancetoTarget = (target.position.y - transform.position.y) / 5;
+
+        transform.Translate((xdistanceToDestiny * cameraToMouseSpeed) * Time.deltaTime, 
+                            (ydistanceToDestiny * cameraToMouseSpeed) * Time.deltaTime, 
+                            0);
+        transform.Translate((xDistancetoTarget * cameraSpeed) * Time.deltaTime, 
+                            (yDistancetoTarget * cameraSpeed) * Time.deltaTime,
+                            0);
     }
 }
